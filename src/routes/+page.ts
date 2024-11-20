9696ba74c16548b384fa55b771db9e5ba55ab366
Fahridanaa <fahridanaaa@gmail.com>
2024-11-20 10:24:08 +0700

    feat: add shadcn components

---
+++ src/routes/+page.ts
@@ -0,0 +1,10 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = () => {
+	return {
+			title: 'Hello world!',
+			content: 'Welcome to our blog. Lorem ipsum dolor sit amet...'
+		};
+	error(404, 'Not found');
+};
