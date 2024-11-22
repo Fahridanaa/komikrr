36ba9e6de5ab60cdfdc77445a2bd92174e2ad63b
Fahridanaa <fahridanaaa@gmail.com>
2024-11-22 15:43:55 +0700

    feat: migrate comic to database

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer, timestamp, jsonb } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
 	id: text('id').primaryKey(),
@@ -12,6 +12,23 @@ export const session = pgTable('session', {
 		.references(() => user.id),
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
+
+export const comic = pgTable('comic', {
+  id: serial('id').primaryKey(),
+  title: text('title').notNull(),
+  author: text('author').notNull(),
+  type: text('type', { enum: ['manga', 'manhwa', 'manhua'] }).notNull(),
+  status: text('status', { enum: ['ongoing', 'completed'] }).notNull(),
+  release: timestamp('release', { withTimezone: true }).notNull(),
+  updatedOn: timestamp('updated_on', { withTimezone: true }).notNull(),
+  genres: jsonb('genres').notNull(),
+  synopsis: text('synopsis').notNull(),
+  coverImageUrl: text('cover_image_url').notNull(),
+  rating: integer('rating'),
+});
+
+export type Comic = typeof comic.$inferSelect;
+export type NewComic = typeof comic.$inferInsert;
 
 export type Session = typeof session.$inferSelect;
 
