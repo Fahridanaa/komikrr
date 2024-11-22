85a6b60402048e84a272dd093d90dee58bf5b83b
Fahridanaa <fahridanaaa@gmail.com>
2024-11-22 16:27:39 +0700

    feat: add component genre list for comic

---
+++ src/lib/components/comic-genre-list/ComicGenreList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Badge } from "$lib/components/ui/badge";
+
+  export let genres: string[];
+</script>
+
+<div class="space-y-2">
+  <h2 class="font-semibold">Genres</h2>
+  <div class="flex flex-wrap gap-2">
+    {#each genres as genre (genre)}
+      <Badge variant="secondary">
+        {genre}
+      </Badge>
+    {/each}
+  </div>
+</div>
